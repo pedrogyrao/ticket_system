4194165dc9535aadbda4a0f874d229a4528c87a8
Pedro Gyrão <pedrogyrao@hotmail.com>
2020-09-17 11:25:20 -0300

    Adding db functions to start and close connection to mongo.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
 from api.api import router as api_router
+from db.mongodb_utils import connect_to_mongo, close_mongo_connection
 
 
 app = FastAPI()
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
 app.include_router(api_router, prefix='')

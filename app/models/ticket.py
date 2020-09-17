f7566e1eb879b50ae1271b4cb4fe37cf4ca46abe
Pedro Gyrão <pedrogyrao@hotmail.com>
2020-09-17 11:35:34 -0300

    Adjusting Models.

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -4,10 +4,23 @@ from pydantic import BaseModel
 
 
 class TicketModel(BaseModel):
-    ticket_id: str
     description: str
     points: int
     sprint_num: int
+    handler: Optional[str] = None
+
+
+class TicketModelInDB(TicketModel):
+    _id: str
+    created_at: Optional[datetime] = None
+    started_at: Optional[datetime] = None
+    finished_at: Optional[datetime] = None
+
+
+class TicketPutModel(BaseModel):
+    description: str = None
+    points: int = None
+    sprint_num: int = None
     handler: Optional[str] = None
     created_at: Optional[datetime] = None
     started_at: Optional[datetime] = None
